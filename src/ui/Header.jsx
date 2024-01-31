ddae9ade08991a09f893e2b51649a2a58ab56ad0
javharbek <yuldashevjavharbek@gmail.com>
2024-01-31 22:09:36 +0500

    adding actions to create order

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
-function Header () {
+function Header() {
   return (
     <header>
-      <Link to='/'>Fast React Pizza Co.</Link>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
