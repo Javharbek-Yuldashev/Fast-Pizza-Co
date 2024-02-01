995a817ee10e84e2db09c5abd7967b39df0b928b
javharbek <yuldashevjavharbek@gmail.com>
2024-02-01 14:26:00 +0500

    set up tailwind

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="flex text-center text-xl text-yellow-500 sm:text-2xl ">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
